02036d08dcaecd1a6de86477db1c2b45bdb0829c
Bereket Urgessa <bereketadugna15@gmail.com>
2021-11-04 04:50:24 -0400

    Add difficultyLevel func, Update generateWord func based on difficulty

--- js/main.js
+++ js/main.js
@@ -47,11 +47,16 @@ let guessed;
 // maximum number of allowed guesses (secret word length)
 let maxGuess = 0;
 
+let difficulty = 1;
+console.log(difficulty);
+
+let difficultyLevels = ["Easy", "Normal", "Hard"];
+
 // generates keyboard layout
 
 const message = document.getElementById("display-result");
 const hint = document.getElementById("display-hint"); // display hint for player
-const hintBtn = document.getElementById("hint"); //
+const hintBtn = document.getElementById("hint");
 
 const keyboard = document.querySelector(".keyboard");
 const guessedChar = document.getElementById("guessed"); // to display correct guesses
@@ -60,10 +65,38 @@ const resetBtn = document.getElementById("reset");
 hintBtn.addEventListener("click", displayHint); // shows hint to player
 resetBtn.addEventListener("click", initialize); // resets the game
 
+const difficultyOption = document.querySelector(".difficulty");
+
+initialize();
+generateKeyboard();
+difficultyLevel();
+//initialize the game
+function initialize() {
+  guessed = [];
+
+  generateWord(); // select random word
+
+  for (let i = 0; i < word.length; i++) {
+    // Set default value for guesses array to _  _ _ _ X length of secret word
+    guessed[i] = "_";
+  }
+  guessedChar.innerHTML = guessed.join(" "); // Display default guessed Array to player
+  wordLeangth = word.length + 1;
+  message.textContent = "Guess To Win";
+  hint.textContent = "";
+  hintBtn.classList.remove("key-disable");
+  keyboard.classList.remove("key-disable");
+  keyboard.childNodes.forEach(function (letter) {
+    // loop over all keyboad elements
+    letter.classList.remove("button-green");
+    letter.classList.remove("key-disable");
+  });
+}
+
 function generateKeyboard() {
   // creates button for each letters (keyboard)
   letters.forEach(function (letter, idx) {
-    const keyboard = document.querySelector(".keyboard");
+    // const keyboard = document.querySelector(".keyboard");
     letter = document.createElement("button");
     letter.classList.add("button");
     letter.textContent = letters[idx];
@@ -112,30 +145,6 @@ function checkWin() {
   }
 }
 
-initialize();
-generateKeyboard(); // generate keyboard and handle move function
-//initialize the game
-function initialize() {
-  guessed = [];
-  generateWord(); // select random word
-
-  for (let i = 0; i < word.length; i++) {
-    // Set default value for guesses array to _  _ _ _ X length of secret word
-    guessed[i] = "_";
-  }
-  guessedChar.innerHTML = guessed.join(" "); // Display default guessed Array to player
-  wordLeangth = word.length + 1;
-  message.textContent = "Guess To Win";
-  hint.textContent = "";
-  hintBtn.classList.remove("key-disable");
-  keyboard.classList.remove("key-disable");
-  keyboard.childNodes.forEach(function (letter) {
-    // loop over all keyboad elements
-    letter.classList.remove("button-green");
-    letter.classList.remove("key-disable");
-  });
-}
-
 function generateWord() {
   const categoryIndex = allWords.indexOf(
     allWords[Math.floor(Math.random() * allWords.length)]
@@ -146,8 +155,33 @@ function generateWord() {
   // words array inside the object
   const wordsArray = allWords[categoryIndex].words;
 
-  // select random word from words array
-  word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+  // leveld words based on thier length
+  let leveledWords = [];
+  if (difficulty === 2) {
+    wordsArray.forEach(function (wordCheck) {
+      if (wordCheck.length >= 7) {
+        leveledWords.push(wordCheck);
+      }
+      return;
+    });
+  } else if (difficulty === 1) {
+    wordsArray.forEach(function (wordCheck) {
+      if (wordCheck.length > 3 && wordCheck.length < 7) {
+        leveledWords.push(wordCheck);
+      }
+      return;
+    });
+  } else if (difficulty === 0) {
+    wordsArray.forEach(function (wordCheck) {
+      if (wordCheck.length <= 3) {
+        leveledWords.push(wordCheck);
+      }
+      return;
+    });
+  }
+  word = leveledWords[Math.floor(Math.random() * leveledWords.length)];
+
+  //   word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 
   maxGuess = word.length; // set maximum guesses to word length
   console.log(maxGuess);
@@ -155,8 +189,28 @@ function generateWord() {
 
 function displayHint() {
   // display hint when hint clicked
-  hint.textContent = `Starts with ${word[0]}`;
+  hint.textContent = `It's a ${wordCategory}`;
   guessed[0] = word[0];
   message.textContent = `${maxGuess} guesses left`;
   guessedChar.textContent = guessed.join("  ");
+}
+
+function difficultyLevel() {
+  difficultyLevels.forEach(function (level, idx) {
+    level = document.createElement("input");
+    level.setAttribute("type", "radio");
+    level.setAttribute("name", "level");
+
+    const label = document.createElement("label");
+    label.setAttribute("for", "level");
+    label.textContent = difficultyLevels[idx];
+
+    difficultyOption.appendChild(level);
+    difficultyOption.appendChild(label);
+
+    level.addEventListener("click", function (select) {
+      difficulty = idx;
+      console.log(difficulty);
+    });
+  });
 }
